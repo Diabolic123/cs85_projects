7b238a18df2921f4304c088e97546cea1a3f0da6
Diabolic123 <your-email@example.com>
2025-06-29 23:34:06 -0700

    Add refactored version and debugging log

--- module2a/price_engine_refactored.php
+++ module2a/price_engine_refactored.php
@@ -20,50 +20,53 @@
             $size = 'XL'; // Options: 'S', 'M', 'L', 'XL'
             $color = 'Ocean Blue'; // Any string, but test with 'Sunset Orange' or 'Ocean Blue'
             $isCustomized = true; // Options: true, false
-            $customerFirstName = 'nicholas'; // <-- IMPORTANT: REPLACE WITH YOUR ACTUAL FIRST NAME
+            $customerFirstName = 'Nicholas'; // <-- IMPORTANT: REPLACE WITH YOUR ACTUAL FIRST NAME
 
             // --- Part A: Implement the logic below using ONLY simple, nested if-statements ---
             $finalPrice = 22.50;
             $details = "<li>Base Price: <span>$" . number_format($finalPrice, 2) . "</span></li>";
 
-            if ($size == 'L') {
+           
+           
+           
+ if ($size == 'L') {
     $finalPrice += 1.75;
     $details .= "<li>Size (L) Upcharge: <span>+$1.75</span></li>";
-}
-
-if ($size == 'XL') {
+} elseif ($size == 'XL') {
     $finalPrice += 2.50;
     $details .= "<li>Size (XL) Upcharge: <span>+$2.50</span></li>";
 }
 
-if ($color == 'Sunset Orange') {
+if ($color == 'Sunset Orange' || $color == 'Ocean Blue') {
     $finalPrice += 2.00;
-    $details .= "<li>Premium Color (Sunset Orange): <span>+$2.00</span></li>";
-}
-if ($color == 'Ocean Blue') {
-    $finalPrice += 2.00;
-    $details .= "<li>Premium Color (Ocean Blue): <span>+$2.00</span></li>";
+    $details .= "<li>Premium Color ($color): <span>+$2.00</span></li>";
 }
 
 if ($isCustomized) {
     $finalPrice += 5.00;
-    $details .= "<li>Customization Fee:(true) <span>+$5.00</span></li>";
-}
+    $details .= "<li>Customization Fee: <span>+$5.00</span></li>";
 
-if ($isCustomized) {
     if ($size == 'XL') {
         $finalPrice += 3.00;
         $details .= "<li>XL Customization Handling Fee: <span>+$3.00</span></li>";
     }
 }
 
-if (strlen($customerFirstName)> 6) {
+if (strlen($customerFirstName) > 6) {
     $finalPrice -= 1.00;
     $details .= "<li>Long Name Discount: <span>-$1.00</span></li>";
 }
 
+/*
+My Debugging Log:
+In part A I two things that I fixed, one was the the customer firstname discount I forgot to put the condition
+where it had to be more than six letters so it would always add the discount. Also I put two seperate if statemnents
+for the colors which made it long so in the refactroed version I used logical OR operator to check both conditions, which 
+cleaned up the code.
+            
 
-            // Your nested if-statement logic goes here...
+
+// Your nested if-statement logic goes here...
             // Example of a rule:
             // if ($size == 'L') {
             //     $finalPrice = $finalPrice + 1.75;
