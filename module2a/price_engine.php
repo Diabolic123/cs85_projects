90260544235c0c3256c30de137d7a9704b1eaf9a
Diabolic123 <your-email@example.com>
2025-06-29 22:57:02 -0700

    Add size XL upcharge

--- module2a/price_engine.php
+++ module2a/price_engine.php
@@ -17,7 +17,7 @@
         <h1>Order Summary</h1>
         <?php
             // --- Configuration: Change these values to test all business rules! ---
-            $size = 'L'; // Options: 'S', 'M', 'L', 'XL'
+            $size = 'XL'; // Options: 'S', 'M', 'L', 'XL'
             $color = 'Ocean blue'; // Any string, but test with 'Sunset Orange' or 'Ocean Blue'
             $isCustomized = true; // Options: true, false
             $customerFirstName = 'Nicholas'; // <-- IMPORTANT: REPLACE WITH YOUR ACTUAL FIRST NAME
@@ -30,6 +30,12 @@
     $finalPrice += 1.75;
     $details .= "<li>Size (L) Upcharge: <span>+$1.75</span></li>";
 }
+
+if ($size == 'XL') {
+    $finalPrice += 2.50;
+    $details .= "<li>Size (XL) Upcharge: <span>+$2.50</span></li>";
+}
+
 
 
             // Your nested if-statement logic goes here...
